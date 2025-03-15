28534c26c3d5ffcf57ca0c00e1cfafe86bf3f05e
Khuong Pham <khuongphampn@gmail.com>
2025-03-16 03:03:16 +0700

    Task 3: Format the output of the application

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -11,4 +12,15 @@ def emotion_detector(text_to_analyze):
         }
     }
     res = requests.post(url=URL, json=JSON_DATA, headers=HEADERS)
-    return res.text
+    # Convert text response to json
+    res_json = json.loads(res.text)
+    emotions = res_json.get('emotionPredictions')[0].get('emotion')
+    # Find highest score
+    max_score = max(emotions.values())
+    # Find Dominant Emotion
+    for name, score in emotions.items():
+        if score == max_score:
+            dominant_emotion = name
+            break
+    emotions['dominant_emotion'] = dominant_emotion
+    return emotions
