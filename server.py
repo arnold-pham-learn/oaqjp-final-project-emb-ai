6578427eae1976eb95bb6c48d1ca0fc97f0fcbf3
Arnold Pham <arnoldpham.learn@gmail.com>
2025-03-20 01:25:27 -0400

    Task 8: Run static code analysis

--- server.py
+++ server.py
@@ -1,3 +1,8 @@
+"""
+    Executing this function initiates the application of sentiment
+    analysis to be executed over the Flask channel and deployed on
+    localhost:5000.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,25 +10,32 @@ app = Flask(__name__)
 
 @app.route("/")
 def render_index_page():
+    """
+        This function initiates the rendering of the main application
+        page over the Flask channel
+    """
     return render_template('index.html')
 
 @app.route("/emotionDetector")
 def sent_emotion_detector():
+    """
+        This code receives the text from the HTML interface and 
+        runs emotion detector over it using emotion_detector()
+        function.
+    """
     text_to_analyze = request.args.get('textToAnalyze')
     analyze_result = emotion_detector(text_to_analyze)
     if not analyze_result.get('dominant_emotion'):
         return 'Invalid text! Please try again!'
-    result = "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(
-        analyze_result.get('anger', ''),
-        analyze_result.get('disgust', ''),
-        analyze_result.get('fear', ''),
-        analyze_result.get('joy', ''),
-        analyze_result.get('sadness', ''),
-        analyze_result.get('dominant_emotion', ''),
-    )
+    result = f"""For the given statement, the system response is
+        'anger': {analyze_result.get('anger', '')}, 
+        'disgust': {analyze_result.get('disgust', '')}, 
+        'fear': {analyze_result.get('fear', '')}, 
+        'joy': {analyze_result.get('joy', '')} 
+        and 'sadness': {analyze_result.get('sadness', '')}. 
+        The dominant emotion is {analyze_result.get('dominant_emotion', '')}.
+    """
     return result
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-
